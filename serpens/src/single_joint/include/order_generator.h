50a43f47166a5c0f16cf6bba92c02bc1e74e6f02
Gabriel Mougard <gabriel.mougard@gmail.com>
2021-03-05 20:23:28 +0000

    Rewrite conf file, header and src for generator

--- serpens/src/single_joint/include/order_generator.h
+++ serpens/src/single_joint/include/order_generator.h
@@ -9,25 +9,37 @@ namespace single_joint {
 namespace {
 class OrderGenerator {
     public:
-        OrderGenerator(ros::NodeHandle *nh, OfflineNodeWatcher *watcher);
+        OrderGenerator(ros::NodeHandle *nh);
         ~OrderGenerator();
-        void run();
-    private:
-        int _msg_id_counter; 
-        float _max_theta_m_p;
-        float _theta_ld_min;
-        float _theta_ld_max;
-        float _theta_ld_resolution;
-
-        ros::NodeHandle *_nh;
-        ros::ServiceClient _order_pub;
-        ros::ServiceServer _feedback_sub;
-        ros::ServiceServer _reset_order;
-        
-        void feedbackHandler(const PositionFeedback::ConstPtr& msg);
-        void publishOrder(PositionOrder *order);
-        PositionOrder generate();
-        PositionOrder next(PositionOrder *sent, PositionFeedback *received);
+        void Run();
+    private: 
+        // Order boundaries
+        float theta_ld_min_;
+        float theta_ld_max_;
+        float theta_ld_resolution_;
+        float theta_l_min_;
+        float theta_l_max_;
+        float theta_l_resolution_;
+        float theta_m_min_;
+        float theta_m_max_;
+        float theta_m_resolution_;
+        float theta_m_p_min_;
+        float theta_m_p_max_;
+        float theta_m_p_resolution_;
+        // Position and Feedback order
+        PositionOrder *position_order_;
+        int position_order_count_;
+        PositionFeedback *position_feedback_;
+        int position_feedback_count_;
+        // ROS related
+        ros::NodeHandle *nh_;
+        ros::ServiceClient position_order_client_;
+        ros::ServiceServer position_feedback_server_;
+        // Methods
+        void FeedbackHandler(PositionFeedback::Request &req, PositionFeedback::Response &res);
+        void SendOrder();
+        PositionOrder Generate();
+        PositionOrder Next(PositionOrder &sent, PositionFeedback &received);
 };
 } // namespace
 } // namespace single_joint
--- serpens/src/single_joint/src/generator/order_generator.cpp
+++ serpens/src/single_joint/src/generator/order_generator.cpp
@@ -1,53 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits>
+
 #include <ros/ros.h>
+
 #include "single_joint/order_generator.h"
 
-OrderGenerator::OrderGenerator(
-    ros::NodeHandle *nh,
-    single_joint_episode_generator::OfflineNodeWatcher *watcher) {
-        _msg_id_counter = 0;       
-        _nh = nh:
-        _nh->getParam("/order_generator/max_theta_m_p", _max_theta_m_p);
-        _nh->getParam("/order_generator/theta_ld_min", _theta_ld_min);
-        _nh->getParam("/order_generator/heta_ld_max", _theta_ld_max);
-        _nh->getParam("/order_generator/theta_ld_resolution", _theta_ld_resolution);
+namespace single_joint {
+namespace {
 
-        // Declare publisher, create publisher 'single_joint_episode_generator' using the 'JointOrder' 
-	    // message file from the 'ros_tutorials_topic' package. The topic name is
-	    // '/single_joint/input_to_rl/joint_order' and the size of the publisher queue is set to 100.
-        _order_pub = _nh->advertise<JointOrder>("/single_joint/input_to_rl/joint_order", 100);
-        _nh->serviceClient<single_joint_episode_generator::SrvTutorial>("ros_tutorial_srv");
-
-        _feedback_sub = _nh->subscribe("/single_joint/actuator_to_input/joint_feedback", 100, OrderGenerator::feedbackHandler);
-        _reset_order_srv = _nh->advertiseService("", OrderGenerator::resetOrder);
-}
-
-void OrderGenerator::run() {
-    //1) The first time it is called, just generate a random input message 
-    //(whose the boundaries are controlled by the parameter server)
-
-
-    //2) 
-}
-
-void OrderGenerator::publishOrder() {
-
-}
-
-void OrderGenerator::feedbackHandler() {
-
-}
-
-void OrderGenerator::resetOrder() {
+OrderGenerator::OrderGenerator(ros::NodeHandle *nh) {      
+    nh_ = nh:
     
+    nh_->getParam("/order_generator/theta_ld_min", theta_ld_min_);
+    nh_->getParam("/order_generator/heta_ld_max", theta_ld_max_);
+    nh_->getParam("/order_generator/theta_ld_resolution", theta_ld_resolution_);
+    nh_->getParam("/order_generator/theta_l_min", theta_l_min_);
+    nh_->getParam("/order_generator/heta_l_max", theta_l_max_);
+    nh_->getParam("/order_generator/theta_l_resolution", theta_l_resolution_);
+    nh_->getParam("/order_generator/theta_m_min", theta_m_min_);
+    nh_->getParam("/order_generator/theta_m_max", theta_m_max_);
+    nh_->getParam("/order_generator/theta_m_resolution", theta_m_resolution_);
+    nh_->getParam("/order_generator/theta_m_p_min", theta_m_p_min_);
+    nh_->getParam("/order_generator/theta_m_p_max", theta_m_p_max_);
+    nh_->getParam("/order_generator/theta_m_p_resolution", theta_m_p_resolution_);
+ 
+
+    position_order_client_ = nh_->serviceClient<PositionOrder>("position_order");
+    position_feedback_server_ = nh_->advertiseService("position_feedback", OrderGenerator::FeedbackHandler);
+
+    position_order_ = NULL;
+    position_order_count_ = 0;
+    position_feedback_ = NULL;
+    position_feedback_count_ = 0;
+
 }
 
-single_joint_episode_generator::JointOrder OrderGenerator::generate() {
+void OrderGenerator::Run() {
+    // Only the first time the node is initialized.
+    if (position_order_ == NULL && position_feedback_ == NULL) {
+        position_order_ = OrderGenerator::Generate();
+        OrderGenerator::SendOrder();
+    }
 
+    ros::spin();
 }
 
-single_joint_episode_generator::JointOrder next(
-    single_joint_episode_generator::JointOrder *sent,
-    single_joint_gz_simulator::JointFeedback *received) {
+void OrderGenerator::SendOrder() {
+    if (position_order_client_.call(&position_order_)) {
+        position_order_count_ += 1;
+    } else {
+        ROS_INFO("The position order service client can't reach the server.");
+    }
+}
+
+void OrderGenerator::FeedbackHandler(PositionFeedback::Request &req, PositionFeedback::Response &res) {
+    position_feedback_->request.stamp = req.stamp;
+    position_feedback_->request.msg_id = req.msg_id;
+    position_feedback_->request.theta_m_update = req.theta_m_update;
+    position_feedback_->request.theta_l_update = req.theta_l_update;
+    position_feedback_->request.should_reset = req.should_reset;
+    
+    ROS_INFO("
+        PositionFeedback received :\n
+        \tstamp : %d,\n
+        \tmsg_id : %d,\n
+        \ttheta_m_update: %f,\n
+        \ttheta_l_update: %f,\n
+        \tshoudle_reset: %d,\n
+    ", (int)req.stamp, (int)req.msg_id, (float)req.theta_m_update,
+    (float)req.theta_l_update, (int)req.should_reset);
+
+    if (req.should_reset) {
+        position_feedback_ = NULL;
+        position_order_ = OrderGenerator::Generate();
+        OrderGenerator::SendOrder();
+    } else {
+        position_order_ = OrderGenerator::Next();
+        OrderGenerator::SendOrder();
+    }
+}
+
+
+PositionOrder OrderGenerator::Generate() {
+    /*
+    Random initialization of the input parameters.
+    - 'theta_ld' : will be fixed during the whole episode ('epsilon' is computed using 'theta_ld' and the moving value of 'theta_l').
+    - 'theta_l' : will be randomly initialized (a joint is not necessarily straight at the beginning) but will vary through time.
+    - 'theta_m' : same as 'theta_l'.
+    - 'theta_m_p' ; we randomly decide of which rotationnal speed we want at the beginning. It may vary through time as well.
+    */
+    float rand_theta_ld = RandomSample(theta_ld_min_, theta_ld_max_, theta_ld_resolution_);
+    float rand_theta_l = RandomSample(theta_l_min_, theta_l_max_, theta_l_resolution_);
+    float rand_theta_m = RandomSample(theta_m_min_, theta_m_max_, theta_m_resolution_);
+    float rand_theta_m_p = RandomSample(theta_m_p_min_, theta_m_p_max_, theta_m_p_resolution_);
+    
+    // set the values of the differential parameters.
+    float theta_l_p = numeric_limits<float>::max();
+    float epsilon = numeric_limits<float>::max();
+    float epsilon_p = numeric_limits<float>::max();
+
+    // return the Service to send    
+    PositionOrder order;
+    order.request.stamp = ros::Time::now();
+    order.request.msg_id = position_order_count_;
+    order.request.theta_ld = rand_theta_ld;
+    order.request.theta_l = rand_theta_l;
+    order.request.theta_l_p = theta_l_p;
+    order.request.theta_m = rand_theta_m;
+    order.request.theta_m_p = rand_theta_m_p;
+    order.request.epsilon = epsilon;
+    order.request.epsilon_p = epsilon_p;
+
+    return order;
+}
+
+PositionOrder OrderGenerator::Next() {
 
         int delta = sent->stamp.nsec - received->stamp.nsec;
         single_joint_episode_generator::JointOrder next;
@@ -63,4 +132,18 @@ single_joint_episode_generator::JointOrder next(
         next.epsilon_p = ((sent->theta_ld - received->theta_l_update) - sent->epsilon) / (float)delta;
         return next;
 
+}
+
+} // namespace
+} // namespace single_joint
+
+float RandomSample(float min_val, float max_val, float resolution) {
+    srand(time(NULL)); //initialize the random seed
+    int l = (int)((max_val - min_val) / resolution);
+    float dist[l];
+    for (int i = 0; i < l; i++) {
+        dist[i] =  min_val + (float)i*resolution;
+    }
+    int rand_idx = rand() % l; //generates a random number between 0 and l
+    return dist[l];
 }
