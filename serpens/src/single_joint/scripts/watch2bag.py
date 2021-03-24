15cddd5cc7f77ff2527afe87cff33ad45d8a08b7
gabrielmougard <gabriel.mougard@gmail.com>
2021-03-24 12:46:32 +0100

    [WATCH2BAG] main function

---
+++ serpens/src/single_joint/scripts/watch2bag.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+The main function for the Watcher node
+"""
+
+import rospy
+
+
+if __name__ == "__main__":
+    # Initialize the node and name it.
+    rospy.init_node("watch2bag")
+    # TODO
