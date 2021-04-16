c16b4967b34f18807df46b99d46eeb6bff8cb717
MaubBoris <mauboussinboris@gmail.com>
2021-04-16 14:54:51 +0200

    [GAZEBO] Functionnal test of single joint

--- serpens/src/single_joint/scripts/test_gazebo.py
+++ serpens/src/single_joint/scripts/test_gazebo.py
@@ -1,18 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
 import rospy
 from sensor_msgs.msg import JointState
-import std_msgs.msg import Float64
+from std_msgs.msg import Float64
 
 def joint_states_callback(data):
     rospy.loginfo("joint states : {}".format(data))
     
 def talk_and_listen():
-    pub = rospy.Publisher('/single_joint/joint1_effort_controller/command', Float64, queue_size=10)
+    pub = rospy.Publisher('/single_joint/link_motor_effort/command', Float64, queue_size=10)
     rospy.Subscriber('/single_joint/joint_states', JointState, joint_states_callback)
 
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10Hz
+    rospy.init_node('test_gazebo')
+    rate = rospy.Rate(1) # 10Hz
     while not rospy.is_shutdown():
-        torque_value = random.uniform(0.0, 2.5) # between 0.0 N.m and 2.5 N.m
+        torque_value = random.uniform(-2.5, 2.5) # between 0.0 N.m and 2.5 N.m
         rospy.loginfo("The torque value is : {}".format(torque_value))
         pub.publish(torque_value)
         rate.sleep()
