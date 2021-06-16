475505006fa978ba46cb979e1d07f6b978217d8a
gabrielmougard <gabriel.mougard@gmail.com>
2021-06-16 16:38:08 +0200

    [TOPOLOGY] New topology

--- serpens/src/single_joint/scripts/ppo/network.py
+++ serpens/src/single_joint/scripts/ppo/network.py
@@ -7,10 +7,12 @@ class MlpPolicy(nn.Module):
         super(MlpPolicy, self).__init__()
         self.action_size = action_size
         self.input_size = input_size
-        self.fc1 = nn.Linear(self.input_size, 24).double()
-        self.fc2 = nn.Linear(24, 24).double()
-        self.fc3_pi = nn.Linear(24, self.action_size).double()
-        self.fc3_v = nn.Linear(24, 1).double()
+        self.fc1 = nn.Linear(self.input_size, 48).double()
+        self.fc2 = nn.Linear(48, 48).double()
+        self.fc3 = nn.Linear(48, 48).double()
+        self.fc4 = nn.Linear(48, 48).double()
+        self.fc4_pi = nn.Linear(48, self.action_size).double()
+        self.fc4_v = nn.Linear(48, 1).double()
         self.tanh = nn.Tanh()
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=-1)
@@ -18,11 +20,15 @@ class MlpPolicy(nn.Module):
     def pi(self, x):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
-        x = self.fc3_pi(x)
+        x = self.relu(self.fc3(x))
+        x = self.relu(self.fc4(x))
+        x = self.fc4_pi(x)
         return self.softmax(x)
 
     def v(self, x):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
-        x = self.fc3_v(x)
+        x = self.relu(self.fc3(x))
+        x = self.relu(self.fc4(x))
+        x = self.fc4_v(x)
         return x
